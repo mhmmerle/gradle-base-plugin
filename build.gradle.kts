735f897837ac0362fd011cc59aac683739729564
Marc Haemmerle <marc.haemmerle@adcubum.com>
2019-09-23 16:47:59 +0200

    Define versioning and publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.3.21")
     application
+    `maven-publish`
+    id("com.palantir.git-version").version("0.12.2")
+    id("com.gradle.build-scan").version("2.4.2")
 }
 
 repositories {
     jcenter()
 }
+
+val gitVersion: groovy.lang.Closure<*> by extra
+version = gitVersion()
+group = "email.haemmerle.test"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -16,6 +23,17 @@ dependencies {
 
 application {
     mainClassName = "email.haemmerle.test.AppKt"
+}
+
+publishing {
+    repositories {
+        maven (url = "https://api.bintray.com/content/mhmmerle/snapshots/")
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
 
 buildScan {

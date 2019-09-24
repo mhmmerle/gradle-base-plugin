2a4cc83455ff75671eb0bb5c0ad9b1b21502aa73
Marc Haemmerle <marc.haemmerle@adcubum.com>
2019-09-24 15:32:34 +0200

    Configured publication to bintray

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ For releasing a version create and push tags in the following format:
  ```
  "v-" + <SemVer Version Number>
  ```
+ 
+ ## Publishing
+ 
+ Publishes to bintray using the following environment variables:
+ 
+ * BINTRAY_USER
+ * BINTRAY_KEY
+ 
+ Make sure to register the api key in bintray and register the variables in travis.
+ 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.3.21")
     application
     `maven-publish`
     id("com.palantir.git-version").version("0.12.2")
     id("com.gradle.build-scan").version("2.4.2")
+    id("com.jfrog.bintray").version( "1.8.4")
 }
 
 repositories {
@@ -13,6 +16,7 @@ repositories {
 val gitVersion: groovy.lang.Closure<String> by extra
 version = gitVersion(mapOf ("prefix" to "v-"))
 group = "email.haemmerle.test"
+description = "Project for testing the CI chain github -> travis -> bintray"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -26,14 +30,29 @@ application {
 }
 
 publishing {
-    repositories {
-        maven (url = "https://api.bintray.com/content/mhmmerle/snapshots/")
-    }
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
         }
     }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+
+    setPublications("maven")
+    publish = true
+
+    pkg (delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "snapshots"
+        name = project.name
+        setLicenses("MIT")
+        vcsUrl = "https://github.com/mhmmerle/test-travis-ci.git"
+        websiteUrl = "https://github.com/mhmmerle/test-travis-ci"
+        issueTrackerUrl = "https://github.com/mhmmerle/test-travis-ci/issues"
+    })
+
 }
 
 buildScan {

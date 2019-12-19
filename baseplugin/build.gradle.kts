6195c016a4e16dd9cf0160afac7cc8f184e72129
Marc Haemmerle <marc.haemmerle@adcubum.com>
2019-12-19 11:44:56 +0100

    Eat your own dog food

--- baseplugin/build.gradle.kts
+++ baseplugin/build.gradle.kts
@@ -1,60 +1,28 @@
-import com.jfrog.bintray.gradle.BintrayExtension
-import groovy.lang.Closure
-
 description = "Plugin implementation"
 
 plugins {
+    id("email.haemmerle.baseplugin")
     `java-gradle-plugin`
-    //id("email.haemmerle.baseplugin").version("1.0.0")
-            id("org.jetbrains.kotlin.jvm").version("1.3.61")
-            id("maven-publish")
-            id("com.jfrog.bintray").version("1.8.4")
-            id("com.palantir.git-version").version("0.12.2")
 }
 
-val gitVersionClosure = extensions.extraProperties.get("gitVersion")
-        if (gitVersionClosure is Closure<*>) {
-            version = gitVersionClosure.call(mapOf("prefix" to "v-"))
-        }
-
-publishing { 
-    publications {
-        create<MavenPublication>("maven") {
-            from(components.findByName("java"))
-        }
-    }
-}
-
-bintray {
-    pkg.vcsUrl = "https://github.com/mhmmerle/${name}.git"
-    pkg.websiteUrl = "https://github.com/mhmmerle/${name}"
-    pkg.issueTrackerUrl = "https://github.com/mhmmerle/${name}/issues"
-
-    user = System.getenv("BINTRAY_USER")
-    key = System.getenv("BINTRAY_KEY")
-    setPublications("maven")
-    publish = true
-    pkg.repo = "snapshots"
-    pkg.name = name
-    pkg.setLicenses("MIT")
+`email-haemmerle-base`{
+    username = "mhmmerle"
 }
 
 gradlePlugin {
     plugins {
         create("basePlugin") {
-            id = "email.haemmerle.baseplugin"
+            id = rootProject.name
             implementationClass = "email.haemmerle.gradle.base.BasePlugin"
         }
     }
 }
 
+
 extensions.getByType<BintrayExtension>().apply {
     setPublications(publications.component1(), "basePluginPluginMarkerMaven")
 }
 
-repositories {
-    gradlePluginPortal()
-}
 
 dependencies {
     implementation(gradleApi())

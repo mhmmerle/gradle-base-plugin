149bd6cc1d342664ef39b7e816539924f50c9bd5
Marc Haemmerle <marc.haemmerle@adcubum.com>
2019-12-19 16:53:20 +0100

    Eat your own dog food v 1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("email.haemmerle.baseplugin").version("1.0.2-5-g089b77c").apply(false)
+    id("email.haemmerle.baseplugin").version("1.0.3").apply(false)
 }
 
 group = "email.haemmerle.baseplugin"
